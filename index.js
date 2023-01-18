539d8fe3f1ca5fbbcf714c2c8e113b00a0ea15cb
Sajeel Nadeem Alam <sa06840@st.habib.edu.pk>
2023-01-19 00:40:37 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let homeEl = document.getElementById("home-el")
+let home_score = 0
+
+let guestEl = document.getElementById("guest-el")
+let guest_score = 0
+
+let buttonReset = document.getElementById("button-reset")
+
+function home_one(){
+    home_score += 1
+    homeEl.textContent = home_score
+}
+
+function home_two(){
+    home_score += 2
+    homeEl.textContent = home_score
+}
+
+function home_three(){
+    home_score += 3
+    homeEl.textContent = home_score
+}
+
+function guest_one(){
+    guest_score += 1
+    guestEl.textContent = guest_score
+}
+
+function guest_two(){
+    guest_score += 2
+    guestEl.textContent = guest_score
+}
+
+function guest_three(){
+    guest_score += 3
+    guestEl.textContent = guest_score
+}
+
+function reset_scores(){
+    homeEl.textContent = 0;
+    guestEl.textContent = 0;
+}
